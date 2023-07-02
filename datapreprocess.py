d9fa964bded702fdd74cc587bc3a833edf8de660
Tzu, Ray. Liu <90384877+aes6669ray@users.noreply.github.com>
2023-07-02 13:53:42 +0800

    Add files via upload

---
+++ datapreprocess.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import random
+from datetime import datetime, timedelta
+
+from urllib.parse import unquote
+
+# url = "https://cos.uch.edu.tw/course_info/JS/CourseDetail.aspx?smtr=1121&cos_id=ET0175%20%20&class=%u7532"
+# decoded_url = unquote(url)
+
+# print(decoded_url)
+
